87f863e5823b70b6a2f10e071b99bc9f0708c609
Markus Hunner <26381538+markhun@users.noreply.github.com>
2023-11-27 13:21:28 +0100

    Add problem instances la05 and la31

--- code/enviroment.py
+++ code/enviroment.py
@@ -1,49 +1,52 @@
 import networkx as nx
 import matplotlib
 import matplotlib.pyplot as plt
+import pathlib
 
-class Enviroment():
 
+class Enviroment:
     def __init__(self, file_name, init_pheromone, min_pheromone):
         self.min_pheromone = min_pheromone
-        #Read Data
+        # Read Data
         self.data = self.__readData(file_name)
         self.num_jobs = len(self.data)
         self.num_machines = len(self.data[0])
-        #Create Graph 
+        # Create Graph
         self.G, self.node_names = self.__buildGraph(init_pheromone)
-
 
     def __readData(self, file_name):
         """
         Reads file and create a first data structure.
         The structure is a Matrix:
-            Line:Jobs 
+            Line:Jobs
             Column: Machines
             Content:time of execution.
 
         returns:
             The matrix.
-        """        
+        """
+
+        problem_instance = pathlib.Path(__file__).parent.resolve()
+        problem_instance = problem_instance / "../test_instances" / file_name
+
         jobs = []
-        with open("../test_instances/" + file_name,'r') as file: 
-            for line in file: 
+        with open(problem_instance, "r") as file:
+            for line in file:
                 machines = {}
                 this_machine = None
-                for j,value in enumerate(line.split()):
-                    if j%2 != 0:
-                        machines.update({this_machine : value})
+                for j, value in enumerate(line.split()):
+                    if j % 2 != 0:
+                        machines.update({this_machine: value})
                     else:
                         this_machine = value
                 jobs.append(machines)
 
-        return jobs #Named as <data> outside this method
-
+        return jobs  # Named as <data> outside this method
 
     def __buildGraph(self, init_pheromone):
         """
         From the matrix returned on __readData() method
-        creates a Direct, Weighted and FullConnected Graph 
+        creates a Direct, Weighted and FullConnected Graph
         using networkx library.
         This library will facilitate working with edges.
         Will be added a virtual inital node (-1,-1) that connects
@@ -52,49 +55,55 @@ class Enviroment():
         returns:
             Graph and all node names.
         """
-        num_nodes = self.num_jobs*self.num_machines + 1 #All machineXjobs pair plus the initial virtual node
+        num_nodes = (
+            self.num_jobs * self.num_machines + 1
+        )  # All machineXjobs pair plus the initial virtual node
 
-        #Create directed and fully connected graph, with weight zero in every edge:
-        unnamed_graph = nx.complete_graph(num_nodes,  nx.DiGraph())
+        # Create directed and fully connected graph, with weight zero in every edge:
+        unnamed_graph = nx.complete_graph(num_nodes, nx.DiGraph())
 
-        #Giving proper names to the nodes:
-        node_names = [(-1,-1)] #Add initial virtual node 
+        # Giving proper names to the nodes:
+        node_names = [(-1, -1)]  # Add initial virtual node
         for job in range(self.num_jobs):
             for machine in range(self.num_machines):
                 node_names.append((job, machine))
-        mapping = {i:nodename for i,nodename in enumerate(node_names)} #mapping: {0:(-1,-1), 1:(0,0), ...}
-        G = nx.relabel_nodes(unnamed_graph,mapping)
-        
-        #Remove Edges that point to the  initial vitual node, 
+        mapping = {
+            i: nodename for i, nodename in enumerate(node_names)
+        }  # mapping: {0:(-1,-1), 1:(0,0), ...}
+        G = nx.relabel_nodes(unnamed_graph, mapping)
+
+        # Remove Edges that point to the  initial vitual node,
         # so it only remains from initial to other nodes and not the other way arround:
         for job in range(self.num_jobs):
             for machine in range(self.num_machines):
-                G.remove_edge((job, machine), (-1,-1))
+                G.remove_edge((job, machine), (-1, -1))
 
-        #Update Pheromone and desirability:
+        # Update Pheromone and desirability:
         for from_job in range(self.num_jobs):
             for from_machine in range(self.num_machines):
-                
-                #Add edge attributes (desirability/pheromone) for edges from virtual node
+                # Add edge attributes (desirability/pheromone) for edges from virtual node
                 execution_time = self.data[from_job][str(from_machine)]
-                desirability = 1/int(execution_time)
-                G[(-1,-1)][(from_job,from_machine)]['pheromone'] = init_pheromone
-                G[(-1,-1)][(from_job,from_machine)]['desirability'] = desirability
+                desirability = 1 / int(execution_time)
+                G[(-1, -1)][(from_job, from_machine)]["pheromone"] = init_pheromone
+                G[(-1, -1)][(from_job, from_machine)]["desirability"] = desirability
 
                 for to_job in range(self.num_jobs):
                     for to_machine in range(self.num_machines):
-                        #Add edge attributes (desirability/pheromone) for edges from operational node
+                        # Add edge attributes (desirability/pheromone) for edges from operational node
                         if from_job == to_job and from_machine == to_machine:
                             pass
                         else:
                             execution_time = self.data[to_job][str(to_machine)]
-                            desirability = 1/int(execution_time)
-                            G[(from_job,from_machine)][(to_job,to_machine)]['pheromone'] = init_pheromone
-                            G[(from_job,from_machine)][(to_job,to_machine)]['desirability'] = desirability
+                            desirability = 1 / int(execution_time)
+                            G[(from_job, from_machine)][(to_job, to_machine)][
+                                "pheromone"
+                            ] = init_pheromone
+                            G[(from_job, from_machine)][(to_job, to_machine)][
+                                "desirability"
+                            ] = desirability
 
-        node_names.remove((-1,-1)) #Remove initial virtual node from names
+        node_names.remove((-1, -1))  # Remove initial virtual node from names
         return G, node_names
-
 
     def getGraph(self):
         """
@@ -102,7 +111,7 @@ class Enviroment():
         pheromones and node names
         """
         return self.G
-    
+
     def getNodeNames(self):
         """
         Returns the node names
@@ -121,45 +130,44 @@ class Enviroment():
     def getEdges(self):
         return [edge for edge in self.G.edges]
 
-    
-    def updatePheromone(
-        self,
-        evaporation_rate,
-        cycle_edge_contribution): 
+    def updatePheromone(self, evaporation_rate, cycle_edge_contribution):
         """
         Simulates the pheromone evaporation
-        at each edge by multipling the 
-        evaporation rate to the old 
+        at each edge by multipling the
+        evaporation rate to the old
         pheromone values.
         And simulates the ants pherommone
-        trails contribution when adding 
-        the sum of the inverse of the 
-        time of the path that passed 
+        trails contribution when adding
+        the sum of the inverse of the
+        time of the path that passed
         through that edge..
         """
         for edge in self.G.edges:
             from_node = edge[0]
             to_node = edge[1]
-            old_pheromone = self.G[from_node][to_node]['pheromone']
-            new_pheromone = cycle_edge_contribution[edge] + (evaporation_rate * old_pheromone)
+            old_pheromone = self.G[from_node][to_node]["pheromone"]
+            new_pheromone = cycle_edge_contribution[edge] + (
+                evaporation_rate * old_pheromone
+            )
             if new_pheromone > self.min_pheromone:
-                self.G[from_node][to_node]['pheromone'] = new_pheromone
+                self.G[from_node][to_node]["pheromone"] = new_pheromone
             else:
-                self.G[from_node][to_node]['pheromone'] = self.min_pheromone    
-
+                self.G[from_node][to_node]["pheromone"] = self.min_pheromone
 
     def calculateMakespanTime(self, path):
         """
         Calculates the optimal time (makespan)
-        for the entry path to the job shop 
+        for the entry path to the job shop
         scheduilling problem.
 
         returns:
             makespam time
         """
-        #Inicialize scheduler
-        machine_task_moments = [] #Represent all the tasks on their moments in wich machine
-        machine_moments = [] #Represents in wich moment of time that machine is
+        # Inicialize scheduler
+        machine_task_moments = (
+            []
+        )  # Represent all the tasks on their moments in wich machine
+        machine_moments = []  # Represents in wich moment of time that machine is
         for i in range(self.num_machines):
             machine_task_moments.append([])
             machine_moments.append(0)
@@ -170,7 +178,7 @@ class Enviroment():
             this_task_time = int(self.data[this_job][str(this_machine)])
             moment = machine_moments[this_machine]
             moment_for_task_not_found = True
-            #Verify in wich moment the task can initiate
+            # Verify in wich moment the task can initiate
             while moment_for_task_not_found:
                 foud_other_machine_with_same_task = False
                 for other_machine in range(self.num_machines):
@@ -183,32 +191,29 @@ class Enviroment():
                                 break
                         except:
                             pass
-                
+
                 if foud_other_machine_with_same_task == False:
-                    #Stops the loop
+                    # Stops the loop
                     moment_for_task_not_found = False
-                    #fill the job time for that machine
+                    # fill the job time for that machine
                     for i in range(this_task_time):
                         machine_task_moments[this_machine].append(this_job)
                     machine_moments[this_machine] = this_task_time + moment + 1
-                    
+
                 else:
-                    #Make the machine wait until the job is done in another machine
-                    machine_task_moments[this_machine].append('-')
-                    moment+=1
-                    
-        #--------->Uncomment next lines if you want to print the Makespan 
+                    # Make the machine wait until the job is done in another machine
+                    machine_task_moments[this_machine].append("-")
+                    moment += 1
+
+        # --------->Uncomment next lines if you want to print the Makespan
         #             for each schedule tested on execution:
-        #for i in range(self.num_machines):
+        # for i in range(self.num_machines):
         #    print(machine_task_moments[i])
 
         machine_execution_lengths = []
         for i in range(self.num_machines):
             machine_execution_lengths.append(len(machine_task_moments[i]))
         return max(machine_execution_lengths)
-
-            
-
 
     def printGraph(self):
         """
@@ -218,27 +223,26 @@ class Enviroment():
             png image: 'code/graph.png'
         """
         options = {
-            'node_color': 'blue',
-            'node_size': 2000,
-            'width': 2.5,
-            'arrowstyle': '-|>',
-            'arrowsize': 20,
+            "node_color": "blue",
+            "node_size": 2000,
+            "width": 2.5,
+            "arrowstyle": "-|>",
+            "arrowsize": 20,
         }
-        matplotlib.use('Agg')
-        
-        #Generating edge labels
+        matplotlib.use("Agg")
+
+        # Generating edge labels
         edge_labels = {}
         for from_node, to_node, edge in self.G.edges(data=True):
-            desirability = round(edge['desirability'], 4)
-            pheromone = round(edge['pheromone'], 3)
-            weight = "(" + str(desirability) + " - " +  str(pheromone)+")"
-            edge_labels.update({(from_node, to_node) : weight})
+            desirability = round(edge["desirability"], 4)
+            pheromone = round(edge["pheromone"], 3)
+            weight = "(" + str(desirability) + " - " + str(pheromone) + ")"
+            edge_labels.update({(from_node, to_node): weight})
 
-        #Generating figure    
-        fig = plt.figure()        
+        # Generating figure
+        fig = plt.figure()
         ax = fig.add_subplot(111)
         pos = nx.spring_layout(self.G)
-        nx.draw_networkx_edge_labels(self.G,pos,edge_labels=edge_labels, font_size=7)
+        nx.draw_networkx_edge_labels(self.G, pos, edge_labels=edge_labels, font_size=7)
         nx.draw_networkx(self.G, pos, arrows=True, ax=ax, **options)
-        fig.savefig('graph.png')
-
+        fig.savefig("graph.png")
